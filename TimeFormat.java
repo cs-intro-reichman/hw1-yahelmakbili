b935a3df7b2ca7f2a3eb132ad6fe80caf3f452bb
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 21:56:08 +0200

    fix

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,47 +1,47 @@
 public class TimeFormat {
 	public static void main(String[] args) {
+
 		// get input
 		String input = args[0];
 
-		// find the min and hours
-		int hours = Integer.parseInt("" + input.charAt(0) + input.charAt(1));
-		int minutes = Integer.parseInt("" + input.charAt(3) + input.charAt(4));
+		// get hours and minutes
+		int hours24 = Integer.parseInt("" + input.charAt(0) + input.charAt(1));
+		int mins = Integer.parseInt("" + input.charAt(3) + input.charAt(4));
 
-		// find AM/PM
+		// suffix
 		String suffix;
-		if (hours >= 12) {
+		if (hours24 >= 12) {
 			suffix = "PM";
 		} else {
 			suffix = "AM";
 		}
 
-		// convert hours
-		int displayHour;
-		if (hours == 0) {
-			displayHour = 12;
-		} else if (hours > 12) {
-			displayHour = hours - 12;
+		// convert
+		int hours12;
+		if (hours24 == 0) {
+			hours12 = 12;
+		} else if (hours24 > 12) {
+			hours12 = hours24 - 12;
 		} else {
-			displayHour = hours;
+			hours12 = hours24;
 		}
-
-		// zero hours
+		// zero hour
 		String hourStr;
-		if (displayHour < 10) {
-			hourStr = "0" + displayHour;
+		if (hours12 < 10) {
+			hourStr = "0" + hours12;
 		} else {
-			hourStr = "" + displayHour;
+			hourStr = "" + hours12;
 		}
 
 		// zero minutes
-		String minuteStr;
-		if (minutes < 10) {
-			minuteStr = "0" + minutes;
+		String minStr;
+		if (mins < 10) {
+			minStr = "0" + mins;
 		} else {
-			minuteStr = "" + minutes;
+			minStr = "" + mins;
 		}
 
-		// final print
-		System.out.println(hourStr + ":" + minuteStr + " " + suffix);
+		// הדפסה סופית
+		System.out.println(hourStr + ":" + minStr + " " + suffix);
 	}
 }
