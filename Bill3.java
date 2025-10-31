e37ea784a0b737f0f6626f2ce55007419eef004a
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 16:52:57 +0200

    Solved Bill3 and tested the code

--- Bill3.java
+++ Bill3.java
@@ -1,6 +1,17 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
+		// get input of 3 names
 		String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		// get input of the total bill
+		int bill = Integer.parseInt(args[3]);
+		// divide and round the bill
+		double share = (double) bill / 3;
+		double roundedShare = Math.ceil(share);
+		// print results
+		System.out.println("Dear " + name1 + "," + name2 + " and " + name3 + ": pay" + " " + roundedShare + " "
+				+ " Shekels each.");
 	}
 }
