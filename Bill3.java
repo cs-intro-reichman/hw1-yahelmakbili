2a0cdcf04d11b751550af10c42414c4c6423dd44
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 17:34:24 +0200

    fix2

--- Bill3.java
+++ Bill3.java
@@ -14,6 +14,6 @@ public class Bill3 {
 		System.out
 				.println(
 						"Dear " + name3 + ", " + name2 + "," + " " + "and " + name1 + ": pay" + " " + roundedShare + " "
-								+ " Shekels each.");
+								+ "Shekels each.");
 	}
 }
