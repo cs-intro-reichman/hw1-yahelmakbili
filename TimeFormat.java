29dbb9327c5fd826b7b5240a22780b67fe7dbba9
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 21:58:10 +0200

    fix

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,14 +1,14 @@
 public class TimeFormat {
 	public static void main(String[] args) {
 
-		// get input
+		// הקלט: "HH:MM"
 		String input = args[0];
 
-		// get hours and minutes
+		// ניקח את השעות והדקות כמספרים
 		int hours24 = Integer.parseInt("" + input.charAt(0) + input.charAt(1));
 		int mins = Integer.parseInt("" + input.charAt(3) + input.charAt(4));
 
-		// suffix
+		// לקבוע AM או PM
 		String suffix;
 		if (hours24 >= 12) {
 			suffix = "PM";
@@ -16,16 +16,20 @@ public class TimeFormat {
 			suffix = "AM";
 		}
 
-		// convert
+		// להמיר לשעון 12 שעות
 		int hours12;
 		if (hours24 == 0) {
+			// 00:xx -> 12:xx AM
 			hours12 = 12;
 		} else if (hours24 > 12) {
+			// 13..23 -> (hour-12):xx PM
 			hours12 = hours24 - 12;
 		} else {
+			// 01..12 נשאר אותו דבר
 			hours12 = hours24;
 		}
-		// zero hour
+
+		// לבנות שעתיים ספרות
 		String hourStr;
 		if (hours12 < 10) {
 			hourStr = "0" + hours12;
@@ -33,7 +37,7 @@ public class TimeFormat {
 			hourStr = "" + hours12;
 		}
 
-		// zero minutes
+		// לבנות שתי ספרות דקות
 		String minStr;
 		if (mins < 10) {
 			minStr = "0" + mins;
@@ -41,7 +45,7 @@ public class TimeFormat {
 			minStr = "" + mins;
 		}
 
-		// הדפסה סופית
+		// הדפסה אחת בלבד בכל המקרים
 		System.out.println(hourStr + ":" + minStr + " " + suffix);
 	}
 }
