14dbd63a0535121c49adc0b385fca88fb3d34899
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 21:51:21 +0200

    fix

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,26 +1,47 @@
-// Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
 		// get input
-		String hour = (args[0]);
+		String input = args[0];
+
 		// find the min and hours
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		// find PM\AM
+		int hours = Integer.parseInt("" + input.charAt(0) + input.charAt(1));
+		int minutes = Integer.parseInt("" + input.charAt(3) + input.charAt(4));
+
+		// find AM/PM
 		String suffix;
 		if (hours >= 12) {
 			suffix = "PM";
 		} else {
 			suffix = "AM";
 		}
-		// print correct suffix, hours and minutes
-		if ((hours < 12 || hours == 00) && (minutes < 10)) {
-			System.out.println(hour + " AM");
-		} else if (hours == 12) {
-			System.out.println(hour + " PM");
-		} else if ((hours > 12) && (minutes < 10)) {
-			hours = (hours - 12);
-			System.out.println(hours + ":0" + minutes + " PM");
+
+		// convert hours
+		int displayHour;
+		if (hours == 0) {
+			displayHour = 12;
+		} else if (hours > 12) {
+			displayHour = hours - 12;
+		} else {
+			displayHour = hours;
 		}
+
+		// zero hours
+		String hourStr;
+		if (displayHour < 10) {
+			hourStr = "0" + displayHour;
+		} else {
+			hourStr = "" + displayHour;
+		}
+
+		// zero minutes
+		String minuteStr;
+		if (minutes < 10) {
+			minuteStr = "0" + minutes;
+		} else {
+			minuteStr = "" + minutes;
+		}
+
+		// final print
+		System.out.println(hourStr + ":" + minuteStr + " " + suffix);
 	}
 }
