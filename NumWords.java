93fe756e0b354907824c6e11e4887662fd176e1f
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-10-31 19:01:14 +0200

    fix2

--- NumWords.java
+++ NumWords.java
@@ -5,10 +5,12 @@ public class NumWords {
 		int num = Integer.parseInt(args[0]);
 		// calculates ones
 		int ones = (num % 10);
+		int num1 = (num / 10);
 		// calculates tens
-		int tens = (num / 10);
+		int tens = (num1 % 10);
+		int num2 = (num1 / 10);
 		// calculates hundreds
-		int hundreds = (num / 100);
+		int hundreds = num2;
 		// print the answer
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
